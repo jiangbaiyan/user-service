0adf0d84a2c294cef6d461f7917ca18569b8860c
jiangbaiyan <jiangbaiyan@jd.com>
2019-12-16 10:16:50 +0800

    重新计算节点名称

--- application/controllers/V1/Resource/Create.php
+++ application/controllers/V1/Resource/Create.php
@@ -30,19 +30,10 @@ class V1_Resource_CreateController extends BaseController
             'parent_resource_id' => 'required|numeric',
             'cur_key'            => 'required'
         ]);
-        $aQueue = [];
+
         $nParentResourceId = $aParams['parent_resource_id'];
         $strCurKey         = $aParams['cur_key'];
-        // 父节点id为0，说明它是根节点
-        if ($nParentResourceId == 0) {
-            $strFullKey = $strCurKey;
-        } else { // 否则它挂载了某个节点下
-            // 递归遍历所有祖先节点，并拼接节点名
-            $this->findParent($nParentResourceId, $aQueue);
-            // '.'为分隔符
-            $strFullKey = implode('.', $aQueue) . '.' .  $strCurKey;
-            $strFullKey = trim($strFullKey, '.');
-        }
+        $strFullKey = ResourceModel::getFullKeyByParentResourceId($nParentResourceId, $strCurKey);
         // 查询节点名是否已经存在
         if (ResourceModel::getResourceByFullKey($strFullKey)['total']) {
             throw new OperateFailedException('resource|resource_key_exists');
@@ -54,36 +45,6 @@ class V1_Resource_CreateController extends BaseController
         ];
         ResourceModel::create($aInsert);
         return Response::apiSuccess();
-    }
-
-
-    /**
-     * 递归处理当前节点的所有祖先节点
-     * @param int $nParentResourceId
-     * @param array $aQueue
-     * @return bool
-     * @throws CoreException
-     * @throws OperateFailedException
-     */
-    private function findParent(int $nParentResourceId, array &$aQueue)
-    {
-        // 如果没有到最顶层节点，递归的获取节点名
-        if (!empty($nParentResourceId)) {
-            // 查询父节点名称
-            $aNode = ResourceModel::getById($nParentResourceId);
-            if (!$aNode['total']) {
-                throw new OperateFailedException("resource|parent_node:{$nParentResourceId}_not_exists");
-            }
-            $aNode = $aNode['data'][0];
-            $strCurKey         = $aNode['cur_key'];
-            $nParentResourceId = $aNode['parent_resource_id'];
-            // 将节点名放到数组的前面，这样层级高的节点就在数组的前面
-            array_unshift($aQueue, $strCurKey);
-            // 尾递归，处理父资源节点
-            $this->findParent($nParentResourceId, $aQueue);
-        } else { // 父节点id为0，说明已经到了最顶层节点，push完顶层节点名称后终止递归
-            return true;
-        }
     }
 
 }
